61902f57fbc2acf9944f45efdeed21ab06c418c5
Marek Zdybel <marek@motta.com.pl>
2018-05-12 14:22:07 +0200

    Finalizacja projektu

--- app/src/main/java/com/example/marek/komunikator/CreateMessageActivity.java
+++ app/src/main/java/com/example/marek/komunikator/CreateMessageActivity.java
@@ -1,13 +1,28 @@
 package com.example.marek.komunikator;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
-public class CreateMessageActivity extends AppCompatActivity {
+public class CreateMessageActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_message);
+    }
+
+    public void onSendMessage(View view) {
+        EditText messageView = (EditText) findViewById(R.id.message);
+        String messageText = messageView.getText().toString();
+        Intent intent = new Intent(Intent.ACTION_SEND);
+//        Intent mailIntent = new Intent(Intent.EXTRA_EMAIL);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, messageText);
+        String chooserTitle = getString(R.string.chooser);
+        Intent chosenIntent = Intent.createChooser(intent, chooserTitle);
+        startActivity(chosenIntent);
     }
 }
